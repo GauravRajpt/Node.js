a8c4ba93e6af50e83f1dabbaa7cc684eb679868f
GauravRajpt <joygrajput@gmail.com>
2023-07-13 12:40:54 +0530

    show file list in console

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-const http= require("http");
-http.createServer((req, res)=>{
-    res.write("hello world");
-    res.end()
-}).listen(4300);
+/*show file list*/
+
+//create a folder with name files
+
+// use path module
+
+const fs= require('fs');
+
+const path= require('path')
+const joinedPath= path.join(__dirname, "files");
+console.log(joinedPath)
+
+// for(let i=0; i<5; i++){
+//     fs.writeFileSync(joinedPath+"/hello"+i,"this is file")
+// }
+
+//read the file
+
+fs.readdir(joinedPath,(err,files)=>{
+files.forEach((item)=>{
+    console.log(item)
+})
+})
