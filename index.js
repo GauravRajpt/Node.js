ee3940029b2e6b039820a937ea42af871eab45a8
GauravRajpt <joygrajput@gmail.com>
2023-07-14 09:18:25 +0530

    crud

--- index.js
+++ index.js
@@ -3,13 +3,26 @@ const fs= require('fs');
 const input = process.argv;
 
 
-//create the file
-if(input[2]=='add'){
-fs.writeFileSync(input[3],input[4]);
-}
-else if(input[2]=='remove'){
-    fs.unlinkSync(input[3])
-}
-else{
-    console.log('invalid command')
-}
+const path= require('path');
+const dirpath= path.join(__dirname,'crud');
+const filepath= dirpath+'/apple.txt';
+//create
+//fs.writeFileSync(filepath,"this is text file")
+
+//read
+// fs.readFile(filepath,'utf8',(err,item)=>{
+//     console.log(item)
+// })
+
+// update
+// fs.appendFile(filepath,"file name is apple.txt",(err)=>{
+//     if(!err) console.log("file is updated")
+// });
+
+//rename
+// fs.rename(filepath, `${dirpath}/fruit.txt`, (err)=>{
+//     if(!err) console.log("filename updated");
+// });
+
+//delete
+fs.unlinkSync(`${dirpath}/fruit.txt`);
