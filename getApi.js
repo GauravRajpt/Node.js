7f982e434d48a151d2353e8b2bc737f1d996c5cf
GauravRajpt <joygrajput@gmail.com>
2023-08-19 23:17:48 +0530

    get data from postman to mongodb

--- getApi.js
+++ getApi.js
@@ -1,11 +1,18 @@
 const express= require('express')
 const db= require('./mongodb.js/mongodb')
+const dbConnection = require('./mongodb.js/mongodb')
 const app= express()
-
+app.use(express.json())
 app.get('/',async (req, res)=>{
     let data= await db();
-    data= await data.find().toArray()
+    data= await data.find().toArray();
     res.send(data)
+})
+
+app.post('/', async(req,res)=>{
+    const data= await dbConnection();
+    const result= await data.insertOne(req.body);
+    res.send(result)
 })
 
 app.listen(5000);
