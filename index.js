55c2a607551e15b29549752973169b1711b2ad15
GauravRajpt <joygrajput@gmail.com>
2023-08-20 17:25:38 +0530

    insert data in mongodb with mongoose

--- index.js
+++ index.js
@@ -1,48 +1,14 @@
-const mongoose= require('mongoose');
-const productSchema= mongoose.Schema(
-    {
-        name:String,
-        price:Number
-    }
-);
-const main= async ()=>{
-    await mongoose.connect('mongodb://127.0.0.1:27017/e-com')
-   
-    const productsModel= mongoose.model('bikes',productSchema);
-    let data= new productsModel({
-        name:'m8',price:9000
-    });
-    const result= await data.save();
+const express= require('express');
+require('./config');
+const bikes= require('./bikes');
+
+const app= express();
+app.use(express.json())
+app.post('/',async (req,res)=>{
+    let data=new bikes(req.body);
+    let result=await data.save()
+    res.send(result);
     console.log(result)
-}
+})
 
-main()
-
-const deletedata= async()=>{
-    const productsModel= mongoose.model('bikes',productSchema);
-    const data= await productsModel.deleteOne({
-        name:'m8'
-    })
-}
-deletedata()
-const finddata= async()=>{
-    const productsModel= mongoose.model('bikes',productSchema);
-    const data= await productsModel.find()
-    console.log(data)
-}
-finddata()
-// const update= async ()=>{
-//     const products= mongoose.model('bikes',productSchema);
-// let data=await products.updateOne(
-//     {
-//         name:'m8'
-//     },
-//     {$set:{
-//         price:800   
-//     }}
-   
-// )
-// console.log(data)
-
-// }
-// update()
+app.listen(5000)
