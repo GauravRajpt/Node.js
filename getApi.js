71cae85bb296d73c629f9f03f906dc82d1aacfab
GauravRajpt <joygrajput@gmail.com>
2023-08-20 11:43:32 +0530

    deleted data of mongodb from param

--- getApi.js
+++ getApi.js
@@ -1,6 +1,7 @@
 const express= require('express')
 const db= require('./mongodb.js/mongodb')
 const dbConnection = require('./mongodb.js/mongodb')
+const mongodb= require('mongodb')
 const app= express()
 app.use(express.json())
 app.get('/',async (req, res)=>{
@@ -22,6 +23,14 @@ app.put('/:name',async(req,res)=>{
         {$set:req.body}
     );
     res.send({result:'updated'})
+})
+
+app.delete('/:id',async (req, res)=>{
+    const data= await dbConnection();
+    const result= await data.deleteOne({
+        _id: new mongodb.ObjectId(req.params.id)
+    })
+    res.send("deleted")
 })
 
 app.listen(5000);
