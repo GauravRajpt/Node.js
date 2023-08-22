5032221691cb64e8986d3d113698f2f72150fa10
GauravRajpt <joygrajput@gmail.com>
2023-08-22 16:35:11 +0530

    with moongoose delte-get-put

--- index.js
+++ index.js
@@ -3,12 +3,39 @@ require('./config');
 const bikes= require('./bikes');
 
 const app= express();
-app.use(express.json())
-app.post('/',async (req,res)=>{
+// app.use(express.json())
+/* app.post('/',async (req,res)=>{
     let data=new bikes(req.body);
     let result=await data.save()
     res.send(result);
-    console.log(result)
+   
+}) */
+
+app.get('/list', async (req, res)=>{
+    let data= await bikes.find();
+    console.log(data)
+    res.send(data)
+})
+
+// app.delete('/:_id', async (req, res)=>{
+//     let data = await bikes.deleteOne(req.params)
+//     console.log(data);
+//     res.send(data)
+// })
+
+app.put('/:_id', async (req, res)=>{
+    let data = await bikes.updateOne(
+        
+            req.params,
+            {
+                $set:{
+                    name:'kesri'
+                }
+            }
+    
+    )
+    console.log(data);
+    res.send(data)
 })
 
 app.listen(5000)
