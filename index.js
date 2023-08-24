92900e3aab51638fae995f27d42fff81ef629f8b
GauravRajpt <joygrajput@gmail.com>
2023-08-24 20:49:54 +0530

    seach data in mongodb, in only name and brnad

--- index.js
+++ index.js
@@ -3,39 +3,20 @@ require('./config');
 const bikes= require('./bikes');
 
 const app= express();
-// app.use(express.json())
-/* app.post('/',async (req,res)=>{
-    let data=new bikes(req.body);
-    let result=await data.save()
-    res.send(result);
-   
-}) */
 
-app.get('/list', async (req, res)=>{
-    let data= await bikes.find();
-    console.log(data)
-    res.send(data)
-})
 
-// app.delete('/:_id', async (req, res)=>{
-//     let data = await bikes.deleteOne(req.params)
-//     console.log(data);
-//     res.send(data)
-// })
-
-app.put('/:_id', async (req, res)=>{
-    let data = await bikes.updateOne(
-        
-            req.params,
+app.get('/search/:key', async (req, res)=>{
+    const data = await bikes.find({
+        $or:[
             {
-                $set:{
-                    name:'kesri'
-                }
-            }
-    
-    )
-    console.log(data);
-    res.send(data)
+            "name":{$regex:req.params.key}
+        },
+        {
+            "brand":{$regex:req.params.key}
+        }
+    ]
+    });
+    console.log(req.params.key);
+    res.send(data);
 })
-
 app.listen(5000)
