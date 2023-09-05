c9148034e962b06a8aaf2321d677e7bf51d65b93
GauravRajpt <joygrajput@gmail.com>
2023-09-05 22:10:00 +0530

    revising

--- index.js
+++ index.js
@@ -1,22 +1,18 @@
-const express= require('express');
-require('./config');
-const bikes= require('./bikes');
+const express= require('express')
+
+
+// const path= require('path').resolve("./index.html");
+// console.log(path)
 
 const app= express();
+// console.log(__filename)
 
+app.use(express.static(__dirname+"/public"))
 
-app.get('/search/:key', async (req, res)=>{
-    const data = await bikes.find({
-        $or:[
-            {
-            "name":{$regex:req.params.key}
-        },
-        {
-            "brand":{$regex:req.params.key}
-        }
-    ]
-    });
-    console.log(req.params.key);
-    res.send(data);
+app.get('/about',(req,res)=>{
+    // res.send(req.query.id)
+    // http://localhost:5000/about?id=33
+    res.sendFile(__dirname+"/public/about.html")
 })
-app.listen(5000)
+
+app.listen(5000);
